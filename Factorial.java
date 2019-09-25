056c09611608afe68d93a1e26a2232cf28cd7ac7
Manik Mehta <manik.vipandeep.mehta@uni-weimar.de>
2019-09-25 15:09:49 +0530

    Completed Factorial Exercise.

--- Factorial.java
+++ Factorial.java
@@ -1,4 +1,17 @@
 
 public class Factorial {
 	//todo: implement a recursive functions whicht gives you the factorial value of a given number
+	public static void main(String[] args){
+       System.out.println("Factorial:"+factorial(5));
+	}
+	public static int factorial(int num){
+		
+		if(num<=1){
+          return 1;
+		}else{
+			
+			return num*factorial(num-1);
+		}
+	}
 }
+
