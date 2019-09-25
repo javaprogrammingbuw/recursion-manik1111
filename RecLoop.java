b157d75cf72e12e1245f55d29c2afaceee23169b
Manik Mehta <manik.vipandeep.mehta@uni-weimar.de>
2019-09-25 18:59:15 +0530

    Completed RecLoop exercise.

--- RecLoop.java
+++ RecLoop.java
@@ -8,8 +8,12 @@ public class RecLoop{
 	//Add another method euclidIterative that computes the euclidean algorithm iterative.
 	//The second method powIterative computes the the base to the power of n in a iterative way.
 	//Add another method powRek which does exactly the same, but by using recursion.
+	public static void main(String[] args){
+		System.out.println(euclidIterative(20,20));
+		System.out.println(powRecursive(4.0,2.0));
+	}
 
-	 public static int euclidRec(int a, int b) {
+	/* public static int euclidRec(int a, int b) {
 		if (a == b) {
 			return a;
 		} else if (b > a) {
@@ -19,10 +23,28 @@ public class RecLoop{
 			return euclidRec(a, b);
 		} else {
 			return euclidRec(a - b, b);
+
+		}*/
+	
+
+	public static int euclidIterative(int a, int b){
+		if (a==b){
+			return a;
+		}else if(a>b){
+			for(int i=1;i<=b;i++){
+				b=a-i;
+				
+			}
+			return b;
+		}else{
+			for(int i=1;i<=a;i++){
+				a=b-i;
+			}
+			return a;
 		}
 	}
 
-    public static double powIterative(final double base, final int n) {
+   /* public static double powIterative(final double base, final int n) {
 		if (n < 0) {
 			System.out.println("(Our version of) power is undefined for negative numbers.");
 			return -1;
@@ -34,5 +56,19 @@ public class RecLoop{
 			}
 			return result;
 		}
+	}*/
+
+	public static double powRecursive(final double base,final double n){
+    	if(n<0){
+    		System.out.println("(Our version of) power is undefined for negative numbers.");
+			return -1;
+    	}
+    	if(n==0){
+    		return 1;
+    	}else {
+
+    		return base*powRecursive(base,n-1);
+    	}
 	}
+
 }
